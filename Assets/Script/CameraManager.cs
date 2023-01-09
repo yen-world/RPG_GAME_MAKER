bb2d23c93eb160bcff69b9705664acaab589b458
yen-world <96435899+yen-world@users.noreply.github.com>
2023-01-10 00:08:57 +0900

    part7

--- Assets/Script/CameraManager.cs
+++ Assets/Script/CameraManager.cs
@@ -8,9 +8,21 @@ public class CameraManager : MonoBehaviour
     public GameObject target;
     public float moveSpeed;
     private Vector3 targetPosition;
-    // Start is called before the first frame update
-    void Start()
-    {
+
+    public BoxCollider2D bound;
+
+    // 박스 콜라이더 영역의 최소, 최대 vector3 값을 지님
+    private Vector3 minBound;
+    private Vector3 maxBound;
+
+    // 카메라의 너비의 반, 높이의 반 값을 지닐 변수
+    private float halfWidth;
+    private float halfHeight;
+
+    private Camera theCamera;
+
+    private void Awake()
+    { // start 메서드보다 awake가 먼저 실행됨
         if (instance != null)
         {
             Destroy(this.gameObject);
@@ -21,6 +33,15 @@ public class CameraManager : MonoBehaviour
             instance = this;
         }
     }
+    // Start is called before the first frame update
+    void Start()
+    {
+        theCamera = GetComponent<Camera>();
+        minBound = bound.bounds.min;
+        maxBound = bound.bounds.max;
+        halfHeight = theCamera.orthographicSize;
+        halfWidth = halfHeight * Screen.width / Screen.height; // 너비의 반을 구하는 공식
+    }
 
     // Update is called once per frame
     void Update()
@@ -30,6 +51,19 @@ public class CameraManager : MonoBehaviour
             targetPosition.Set(target.transform.position.x, target.transform.position.y, this.transform.position.z); // this : 스크립트가 적용된 객체(즉, this = 카메라)
 
             this.transform.position = Vector3.Lerp(this.transform.position, targetPosition, moveSpeed * Time.deltaTime);
+
+            float clampedX = Mathf.Clamp(this.transform.position.x, minBound.x + halfWidth, maxBound.x - halfWidth);
+            float clampedY = Mathf.Clamp(this.transform.position.y, minBound.y + halfHeight, maxBound.y - halfHeight);
+
+            this.transform.position = new Vector3(clampedX, clampedY, this.transform.position.z);
+
         }
+    }
+
+    public void SetBound(BoxCollider2D newBound)
+    {
+        bound = newBound;
+        minBound = bound.bounds.min;
+        maxBound = bound.bounds.max;
     }
 }
--- Assets/Script/TransferScene.cs
+++ Assets/Script/TransferScene.cs
@@ -2,15 +2,11 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
-
-public class TransferMap : MonoBehaviour
+public class TransferScene : MonoBehaviour
 {
-    public Transform target;
-    public BoxCollider2D targetBound;
-
+    public string transferMapName;
     private MovingObject thePlayer;
     private CameraManager theCamera;
-
     // Start is called before the first frame update
     void Start()
     {
@@ -18,14 +14,17 @@ public class TransferMap : MonoBehaviour
         thePlayer = FindObjectOfType<MovingObject>();
     }
 
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
     private void OnTriggerEnter2D(Collider2D other)
     {
         if (other.gameObject.name == "Player")
         {
-            theCamera.SetBound(targetBound);
-            theCamera.transform.position = new Vector3(target.transform.position.x, target.transform.position.y, theCamera.transform.position.z);
-            thePlayer.transform.position = target.transform.position;
+            thePlayer.currentMapName = transferMapName;
+            SceneManager.LoadScene(transferMapName);
         }
     }
 }
-
